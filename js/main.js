1aca60989b9b40d835919de33863fa13525da4a7
Thomas <thomasmadariaga2021@outlook.es>
2024-11-18 21:23:35 -0300

    optimizacion js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-$(window).on("load", function() {
-    setTimeout(() => {
-        $(".pantalla_carga").addClass("hidden");
-    }, 1000); 
-});
+const productosContenedor = document.getElementById("productos");
+import { renderizarProductos, obtenerProductos } from "./tarjetas.js";
+
+
+async function init() {
+    try {
+        // obtener productos api
+        const productosData = await obtenerProductos ()
+
+        // renderizar los productos al dom
+        renderizarProductos(productosData, productosContenedor);       
+    } catch (error) {
+        console.error("Error al inicializar la aplicación:", error);
+    }
+}
+
+// Ejecutar la inicialización al cargar la página
+document.addEventListener("DOMContentLoaded", init);
--- js/tarjetas.js
+++ js/tarjetas.js
@@ -1,46 +1,44 @@
-let productos = document.getElementById("productos");
-let url = 'https://fakestoreapi.com/products';
-
+let url = "https://fakestoreapi.com/products";
 
 export async function obtenerProductos() {
     try {
         const response = await fetch(url);
-        const data = await response.json();
+        const data = await response.json ()
         return data;
     } catch (error) {
         console.error("Error al obtener productos:", error);
-        return [];
     }
 }
-export async function renderizarProductos() {
+
+
+export async function renderizarProductos(data, productosContenedor) {
     let productosHTML = `
     <div class="container">
         <div class="row">
-`;
+    `;
 
-data.forEach(producto => {
-    productosHTML += `
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-        <div class="tarjetas">
-            <img src="${producto.image}" class="card-img-top" alt="${producto.title}">
-            <div class="tarjeta_cuerpo">
-                <h5 class="tarjeta_titulo">${producto.title}</h5>
-                <p class="tarjeta_texto">${producto.description.slice(0, 100)}...</p>
-                <p class="precio">$${producto.price}</p>
-                <div>
-                <label for="cantidad-${producto.id}">Cantidad de productos:</label>
-                <input type="number" id="cantidad-${producto.id}" name="cantidad" value="1" min="1" max="100" step="1">
-                <button data-id="${producto.id}">Agregar al carrito</button>
+    data.forEach(producto => {
+        productosHTML += `
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div class="tarjetas">
+                <img src="${producto.image}" class="card-img-top" alt="${producto.title}">
+                <div class="tarjeta_cuerpo">
+                    <h5 class="tarjeta_titulo">${producto.title}</h5>
+                    <p class="tarjeta_texto">${producto.description.slice(0, 100)}...</p>
+                    <p class="precio">$${producto.price}</p>
+                    <div>
+                    <label for="cantidad-${producto.id}">Cantidad de productos:</label>
+                    <input type="number" id="cantidad-${producto.id}" name="cantidad" value="1" min="1" max="100" step="1">
+                    <button data-id="${producto.id}">Agregar al carrito</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-`;
-
-});
-productosHTML += `
+        `;
+    });
+    productosHTML += `
         </div>
     </div>
-`;
-productos.innerHTML = productosHTML;
-}
+    `;
+    productosContenedor.innerHTML = productosHTML;
+};
